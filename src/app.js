3d9fcf17213e1a04ade9c1b05f91758a7488a15d
pixrilic <pixrilic@gmail.com>
2026-07-19 15:22:02 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,27 @@
+// src/App.jsx
 import React from 'react';
 import { BuilderProvider } from './context/BuilderContext';
-import TopToolbar from './components/Toolbar/TopToolbar';
-import LeftSidebar from './components/Sidebars/LeftSidebar';
-import RightSidebar from './components/Sidebars/RightSidebar';
-import CanvasArea from './components/Canvas/CanvasArea';
+import TopToolbar from './components/Layout/TopToolbar';
+import LeftSidebar from './components/Layout/LeftSidebar';
+import RightSidebar from './components/Layout/RightSidebar';
+import Canvas from './components/Layout/Canvas';
 
-function App() {
+export default function App() {
   return (
-    <BuilderProvider>
-      <div className="h-screen w-screen flex flex-col overflow-hidden bg-slate-900 text-slate-100">
+    <BuilderProvider siteId="sample-site-id">
+      <div className="h-screen w-screen flex flex-col overflow-hidden bg-slate-950 font-sans text-slate-200 antialiased selection:bg-blue-500/30">
+        {/* Dynamic Abstract Decorative Lighting Gradients */}
+        <div className="absolute top-[-10%] left-[-10%] w-[50%] h-[50%] rounded-full bg-blue-500/10 blur-[120px] pointer-events-none" />
+        <div className="absolute bottom-[-10%] right-[-10%] w-[50%] h-[50%] rounded-full bg-indigo-500/10 blur-[120px] pointer-events-none" />
+
         <TopToolbar />
-        <div className="flex-1 flex overflow-hidden">
+        
+        <div className="flex-1 flex overflow-hidden relative z-10">
           <LeftSidebar />
-          <CanvasArea />
+          <Canvas />
           <RightSidebar />
         </div>
       </div>
     </BuilderProvider>
   );
 }
-
-export default App;
